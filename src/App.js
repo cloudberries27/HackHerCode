7f59a51769d741e5c83c986191a21ccf99620d17
cloudberries27 <ccrod2298@gmail.com>
2020-02-08 16:24:19 -0500

    Added routes and pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,101 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Route, BrowserRouter, Redirect } from 'react-router-dom';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
 
 import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/SignUp';
+import Profile from './Pages/Profile';
+import Header from '';
+
+const firebaseConfig = {
+
+}
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(()=> {
+    if(!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+    }
+    firebase
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .catch(function (error) {
+        console.log('error', error);
+    });
+  }, [firebaseConfig]);
+
+  useEffect(()=>{
+    firebase.auth().onAuthStateChanged(function(user){
+      if (user){
+        setLoggedIn(true);
+        setUser(user);
+      }else{
+        setLoggedIn(false);
+        setUser({});
+      }
+    });
+  }, [])
+
+  function signUpFunction(e){
+    e.preventDefault();
+    let email = e.currentTarget.createEmail.value;
+    let password = e.currentTarget.createPassword.value;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function(response){
+        setLoggedIn(true);
+      })
+      .catch(function(error) {
+        console.log('error',error);
+    });
+  }
+  function logInFunction(e){
+    e.preventDefault();
+    let email = e.currentTarget.loginEmail.value;
+    let password = e.currentTarget.loginPassword.value;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function(response){
+        setLoggedIn(true);
+      })
+      .catch(function(error) {
+        console.log('error',error);
+    });
+  }
+  function logoutFunction(){
+    firebase
+      .auth()
+      .signOut()
+      .then(function(response){
+        setLoggedIn(false);
+      })
+      .catch(function(error) {
+        console.log('error',error);
+    });
+  }
   return (
     <div className="App">
+      <Header loggedIn={loggedIn} logoutFunction={logoutFunction}/>
       <BrowserRouter>
         <Route exact path="/">
-            <Home/>
+          { loggedIn ? <Profile user = {user} /> : <Home/> }
+        </Route>
+        <Route exact path="/sign-up">
+          { loggedIn ?  <Redirect to='/' /> : <Signup signUpFunction = {signUpFunction}/> }
+        </Route>
+        <Route exact path="/login">
+          {loggedIn ? <Redirect to='/' /> : <Login logInFunction = {logInFunction}/> }
+        </Route>
+        <Route exact path="/profile">
+        { loggedIn ?  <Profile user = {user} /> : <Login logInFunction={logInFunction}/> }
         </Route>
       </BrowserRouter>
     </div>
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header({loggedIn, logoutFunction}){
+    return(
+        <header>
+            {/* <img src={Logo} alt='logo'/> */}
+            <nav>
+                {loggedIn && <a onClick={() =>logoutFunction()}>Log Out</a>}
+                {loggedIn && <a href = "/make-post">Post</a>}
+                {loggedIn && <a href='/'>Home</a>}
+                {!loggedIn &&<a href='/login'>Login</a>}
+                {!loggedIn && <a href='/sign-up'>Sign Up</a>}
+            </nav>
+        </header>
+    )
+}
